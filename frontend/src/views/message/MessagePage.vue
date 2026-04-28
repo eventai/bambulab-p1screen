01a6ccba68482dae0593fd7dcaaf78c3d673ca6a
gaosen <0x5e@sina.cn>
2026-04-28 22:14:33 +0800

    add hms resources

--- frontend/src/views/message/MessagePage.vue
+++ frontend/src/views/message/MessagePage.vue
@@ -5,6 +5,11 @@
 </template>
 
 <script setup lang="ts">
+/*
+"hms":[{"attr":117448704,"code":131073,"action":0,"timestamp":1777383334}]}
+AMS耗材用尽。请把耗材放入同一个AMS槽位后继续。
+[0700-8011 213628]
+*/
 </script>
 
 <style scoped>
